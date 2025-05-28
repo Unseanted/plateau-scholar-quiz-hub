8f140f82016e84dbee2dec7ad6b673d3a40b44de
Sean Samuel <108523927+Unseanted@users.noreply.github.com>
2025-05-28 13:28:18 +0100

    Set up NestJS backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ApplicationsModule } from './applications/applications.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    AuthModule,
+    UsersModule,
+    ApplicationsModule,
+  ],
+})
+export class AppModule {}
